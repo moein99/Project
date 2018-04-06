b8c2354d28785b7655cd8815256530a0620037b6
moein99 <moein.mirzaei95@gmail.com>
2018-04-06 16:19:57 +0430

    methode addRequest baraye voroodi task 2 ezafe shod, bazi az bug ha ok shod

--- src/Config.java
+++ src/Config.java
@@ -1,3 +1,9 @@
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class Config
 {
     public static String menu_link = "https://snapp-food.com/restaurant/menu/load";
@@ -10,4 +16,26 @@ public class Config
     public static String location_link = "https://snappfood.ir/restaurant/location";
     public static String baseUrl = "https://snappfood.ir";
     public static String address_link = "https://snappfood.ir/restaurant/city/";
+
+    public static Document getDocument(String url)
+    {
+        Document document;
+        try
+        {
+            document = Jsoup.connect(url).get();
+            return document;
+        }
+        catch (java.net.SocketTimeoutException e)
+        {
+            System.out.println("Time out exception ...! \nRunning again ...!");
+            return getDocument(url);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
 }
